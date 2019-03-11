fe4fa363248833076531fc37be53388d7cba711f
Enrique Hernandez <hernandez.x.enrique@gmail.com>
2019-03-10 22:04:17 -0500

    add snake to canvas

--- js/snake.js
+++ js/snake.js
@@ -3,12 +3,22 @@
 /** CONSTANTS **/
 const CANVAS_BORDER_COLOR = 'black';
 const CANVAS_BACKGROUND_COLOR = 'white';
+const SNAKE_COLOR = 'lightgreen';
+const SNAKE_BORDER_COLOR = 'darkgreen';
 
 // Get canvas element
 let gameCanvas = document.getElementById("gameCanvas");
 
 // Return two dimensional drawing context
 let context = gameCanvas.getContext("2d");
+
+let snake = [
+    {x: 150, y: 150},
+    {x: 140, y: 150},
+    {x: 130, y: 150},
+    {x: 120, y: 150},
+    {x: 110, y: 150}
+    ];
 
 // Select canvas color
 context.fillStyle = CANVAS_BACKGROUND_COLOR;
@@ -21,3 +31,17 @@ context.fillRect(0, 0, gameCanvas.width, gameCanvas.height);
 
 // Draw a "border" around the entire canvas
 context.strokeRect(0, 0, gameCanvas.width, gameCanvas.height);
+
+function drawSnakePart(snakePart) {
+    context.fillStyle = SNAKE_COLOR;
+    context.strokeStyle = SNAKE_BORDER_COLOR;
+
+    context.fillRect(snakePart.x, snakePart.y, 10, 10);
+    context.strokeRect(snakePart.x, snakePart.y, 10, 10);
+}
+
+function drawSnake() {
+    snake.forEach(drawSnakePart)
+}
+
+drawSnake();
