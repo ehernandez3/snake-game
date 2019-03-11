c0e6d7229fdac9b22cdf08f6b007deb0c2ac6ffc
Enrique Hernandez <hernandez.x.enrique@gmail.com>
2019-03-10 20:45:37 -0500

    gave canvas fill color and border

--- js/snake.js
+++ js/snake.js
@@ -4,5 +4,20 @@
 const CANVAS_BORDER_COLOR = 'black';
 const CANVAS_BACKGROUND_COLOR = 'white';
 
-// Get the canvas element
-var gameCanvas
+// Get canvas element
+let gameCanvas = document.getElementById("gameCanvas");
+
+// Return two dimensional drawing context
+let context = gameCanvas.getContext("2d");
+
+// Select canvas color
+context.fillStyle = CANVAS_BACKGROUND_COLOR;
+
+// Select canvas border color
+context.strokeStyle = CANVAS_BORDER_COLOR;
+
+// Draw a "filled" rectangle to cover the entire canvas
+context.fillRect(0, 0, gameCanvas.width, gameCanvas.height);
+
+// Draw a "border" around the entire canvas
+context.strokeRect(0, 0, gameCanvas.width, gameCanvas.height);
