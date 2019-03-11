0cc1c5a6e3f15dab88ce67209ab77f7b0640e91d
Enrique Hernandez <hernandez.x.enrique@gmail.com>
2019-03-10 22:59:57 -0500

    add comments to existing code

--- js/snake.js
+++ js/snake.js
@@ -6,19 +6,25 @@ const CANVAS_BACKGROUND_COLOR = 'white';
 const SNAKE_COLOR = 'lightgreen';
 const SNAKE_BORDER_COLOR = 'darkgreen';
 
-// Get canvas element
-let gameCanvas = document.getElementById("gameCanvas");
 
-// Return two dimensional drawing context
-let context = gameCanvas.getContext("2d");
-
-let snake = [
+/** Snake made up of an array of objects.
+* Each object contains a set of coordinates
+* that represent a part of the snake.
+*/
+ let snake = [
     {x: 150, y: 150},
     {x: 140, y: 150},
     {x: 130, y: 150},
     {x: 120, y: 150},
     {x: 110, y: 150}
     ];
+
+
+// Get canvas element
+let gameCanvas = document.getElementById("gameCanvas");
+
+// Return two dimensional drawing context
+let context = gameCanvas.getContext("2d");
 
 // Select canvas color
 context.fillStyle = CANVAS_BACKGROUND_COLOR;
@@ -32,15 +38,31 @@ context.fillRect(0, 0, gameCanvas.width, gameCanvas.height);
 // Draw a "border" around the entire canvas
 context.strokeRect(0, 0, gameCanvas.width, gameCanvas.height);
 
+/**
+ * Draws part of snake on canvas
+ * @param snakePart - Coordinates where part should be drawn
+ */
 function drawSnakePart(snakePart) {
+    // Set snake color
     context.fillStyle = SNAKE_COLOR;
+
+    // Set snake border color
     context.strokeStyle = SNAKE_BORDER_COLOR;
 
+    // Draws and fills rectangle representing a part of the snake
+    // at whatever coordinates are passed
     context.fillRect(snakePart.x, snakePart.y, 10, 10);
+
+    // Draws border around snake part at whatever coordinates are
+    // passed
     context.strokeRect(snakePart.x, snakePart.y, 10, 10);
 }
 
+/**
+ * Draws entire snake on canvas
+ */
 function drawSnake() {
+    // Loops through snake parts drawing each part to canvas
     snake.forEach(drawSnakePart)
 }
 
